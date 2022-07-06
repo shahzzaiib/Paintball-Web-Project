fe3d42b9c64c2f7b5b61d450d05e1fab5a9b0abb
Shahzaib Mudassar <smudasar66@gmail.com>
2022-07-06 18:01:55 +0500

    Add project files.

---
+++ Controllers/Aboutus.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Paint_web_project.Controllers
+{
+    public class Aboutus : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Paint_web_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
